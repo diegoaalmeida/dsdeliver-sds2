c40aa7d205594077a8525acf437892f2a8f7632a
Diego Almeida <diegoangelo.almeida@gmail.com>
2021-01-07 20:19:44 -0300

    GET /products

--- backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -12,9 +12,6 @@ import javax.persistence.Table;
 @Table(name = "tb_product")
 public class Product implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	@Id
